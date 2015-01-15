a2924a2481e069ac2d9d03dff2c0b1405cf41ce2
art.dahm <art.dahm@lge.com>
2015-01-15 12:22:38 -0800

    Bumped version to 0.5.0

--- Clock.ino
+++ Clock.ino
@@ -1,6 +1,6 @@
 /*
  * SmartMatrix Clock
- * Version 0.4.2
+ * Version 0.5.0
  * Copyright (c) 2014 Art Dahm (art@dahm.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
